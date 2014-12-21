2c939f069819ea985eff64c597a504536911bcad
Kevin <kaveman1105@gmail.com>
2014-12-21 14:40:42 -0800

    loading html file but no graph

--- app/src/main/java/anderson/k/lifecycle/MainActivity.java
+++ app/src/main/java/anderson/k/lifecycle/MainActivity.java
@@ -1,18 +1,14 @@
 package anderson.k.lifecycle;
 
 import android.annotation.SuppressLint;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.app.Activity;
 
 @SuppressLint("SetJavaScriptEnabled")
 public class MainActivity extends ActionBarActivity {
@@ -38,7 +34,7 @@ public class MainActivity extends ActionBarActivity {
         mywebview.addJavascriptInterface(new WebAppInterface(), "Android");
         websettings.setJavaScriptEnabled(true);
 
-        mywebview.loadUrl("file:///assets/chart.html");
+        mywebview.loadUrl("/assets/chart.html");
     }
 
 
